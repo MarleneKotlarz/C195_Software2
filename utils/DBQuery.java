d96137fd95671c0e05faeb6fb1f342a15c7ec612
MarleneKotlarz <33337266+MarleneKotlarz@users.noreply.github.com>
2020-06-02 12:38:35 -0400

    Added update and delete methods
    Still working on the implementation of the methods. Update method shows error that parameter 3 and 6 are not found and delete does not delete all fields correctly.

--- utils/DBQuery.java
+++ utils/DBQuery.java
@@ -11,6 +11,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -92,7 +94,7 @@ public class DBQuery {
                 cityId=rs.getString("cityId"); // getting the SQL results which is the cityId column
                 return cityId;
             }
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println("Check your SQL");        
         }
         return null;
@@ -100,7 +102,7 @@ public class DBQuery {
     
 
 ////////// get addressId //////////    
-    public static String getAddressIdFromLastCostumerAdded() {
+    public static String getAddressIdFromLastCustomerAdded() {
     String sqlStmt = "SELECT addressId FROM address Order By addressId DESC Limit 1"; // ? is a placeholder value
         rs = null;
         String addressId;
@@ -113,12 +115,43 @@ public class DBQuery {
                 addressId=rs.getString("addressId"); // getting the SQL results which is the addressId column
                 return addressId;
             }
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println("Check your SQL");        
         }
         return null;
     }
-
+    
+////////// get Customer Info for Tableview //////////
+public static ObservableList<Customer> getAllCustomers() {
+    String sqlStmt = "SELECT customerId, customerName, address, address2, city, postalCode, country, phone\n" +
+        "FROM customer cu\n" + 
+        "INNER JOIN address ad ON cu.addressId = ad.addressId\n" +
+        "INNER JOIN city ci ON ad.cityId = ci.cityId\n" +
+        "INNER JOIN country co ON ci.countryId = co.countryId ORDER BY customerId";
+    try {
+        conn = DBConnection.getConnection();
+        ps = conn.prepareStatement(sqlStmt);
+        rs = ps.executeQuery(); // submits entire SQL statement
+            
+        while(rs.next()) {
+            String id = rs.getString("customerId");
+            String name = rs.getString("customerName");
+            String add = rs.getString("address");
+            String add2 = rs.getString("address2");
+            String city = rs.getString("city");
+            String zip = rs.getString("postalCode");
+            String country = rs.getString("country");
+            String phone = rs.getString("phone");
+            // add variables to the customerList for the Customer tableview in Customer controller                
+            customerList.add(new Customer(id, name, add, add2, city, zip, country, phone));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return customerList;        
+    }
+       
+    
     
 ////////// Add Address //////////
     public static String addAddress(String address, String address2, String cityId, String postalCode, String phone) { // passing in addressDB table input fields
@@ -135,7 +168,7 @@ public class DBQuery {
   
             ps.execute(); // submits entire SQL statement
 
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e.getMessage()); 
         }
         return null;
@@ -151,47 +184,84 @@ public class DBQuery {
             ps.setString(1, customerName); // replaces first ? in SQL statement
             ps.setString(2, addressId);             
             ps.execute(); // submits entire SQL statement
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e.getMessage()); 
         }
         return null;
     }    
-    
-    
-//    INSERT INTO `U04YYf`.`customer`
-//(`customerName`,
-//`addressId`,
-//`active`,
-//`createDate`,
-//`createdBy`,
-//`lastUpdate`,
-//`lastUpdateBy`)
-    
-////////// get allCustomers //////////
-//public static ObservableList<Customer> getAllCostumers() {
-//    String sqlStmt = "Select customerId, customerName, address, address2, city, postalCode, country, phone"
-//}       
-     
-}   
-    
-        
-
-    
-    
-    
-    
-    
-
-
-
-
    
     
+////////// Update Customer //////////
+    public static String updateCustomer(String customerName, String addressId) {
+        String sqlStmt = "UPDATE customer SET customerName = ?, addressId = ?, active = '1', createDate = now(), createdBy = 'test', lastUpdate = now(), lastUpdateBy = 'test' WHERE customerId = ?";
     
+        try {
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(sqlStmt);
+            ps.setString(1,customerName);
+            ps.setString(2, addressId);
+            ps.executeUpdate(); // submits update 
+        } catch (SQLException e) {
+            System.out.println("update customer sql error");
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }    
+
     
-    
-    
-    
+////////// Update Address //////////
+    public static String updateAddress(String address, String address2, String cityId, String postalCode, String phone ) {
+        String sqlStmt = "UPDATE address SET address = ?, address2 = ?, cityId = ?, postalCode = ?, phone = ?, createDate = now(), createdBy = 'test', lastUpdate = now(), lastUpdateBy = 'test' WHERE addressId = ?";
+        try {
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(sqlStmt);
+            ps.setString(1,address);
+            ps.setString(2, address2);
+            ps.setString(3, cityId);
+            ps.setString(4, postalCode); 
+            ps.setString(5, phone);             
+            ps.executeUpdate(); // submits update 
+        } catch (SQLException e) {
+            System.out.println("update address sql error");
+            System.out.println(e.getMessage());
+        }
+        return null;        
+    }
     
     
 
+    
+    
+////////// Delete Address //////////    
+    public static String deleteAddress(String addressId) {
+        String sqlStmt = "DELETE FROM address WHERE addressId = ?";
+        try {
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(sqlStmt);
+            ps.setString(1, addressId);
+            ps.executeUpdate(); // submits update 
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+        }
+
+////////// Delete Customer //////////    
+    public static String deleteCustomer(String customerId) {
+        String sqlStmt = "DELETE FROM customer WHERE customerId = ?";
+        try {
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(sqlStmt);
+            ps.setString(1, customerId);
+            ps.executeUpdate(); // submits update 
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+        }    
+   
+    
+}
+    
+    
+    
