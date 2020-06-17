1ef501e695bd7c13d9eaddbac9e1ee883bbc473b
MarleneKotlarz <33337266+MarleneKotlarz@users.noreply.github.com>
2020-06-17 17:36:35 -0400

    Added report variables

--- Model/Report.java
+++ Model/Report.java
@@ -10,5 +10,82 @@ package Model;
  * @author Marlene
  */
 public class Report {
+    String month; 
+    String typeCount; 
+    String type;
+    String customerId;
+    String customerName;
+    String count;
+
+    
+    // Constructor for appt type count
+    public Report(String month, String typeCount, String type) {
+        this.month = month;
+        this.typeCount = typeCount;
+        this.type = type;
+    }
+
+    public Report(String count) {
+        this.count = count;
+    }
+    
+    
+    
+    
+    
+    // Getter and Setter
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getTypeCount() {
+        return typeCount;
+    }
+
+    public void setTypeCount(String typeCount) {
+        this.typeCount = typeCount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+    
+    
+    
     
 }
+
+
